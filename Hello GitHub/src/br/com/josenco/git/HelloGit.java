7b9e554ff9bcc8e19d93817d71f1b894d89bb68b
Joseneide Antonia Santos <164437977+Josenco@users.noreply.github.com>
2024-05-06 21:52:30 -0300

    Adicionei arquivos via upload

---
+++ Hello GitHub/src/br/com/josenco/git/HelloGit.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package br.com.josenco.git;
+
+/**
+ *@Aluna Joseneide Santos args
+ * Exemplo de integração do Eclipse com o GitHub 
+ */
+public class HelloGit {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Git"); 	 
+		
+
+	}
+
+}
